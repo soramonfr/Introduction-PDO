6e151b75f4d9cc00fc316e45961534f299b06936
sorayacruchet <soraya.cruchet@gmail.com>
2021-02-09 09:27:51 +0100

    decom

--- Controllers/indexController.php
+++ Controllers/indexController.php
@@ -10,20 +10,20 @@ $database = new Database();
 // Contient connexion PDO
 $clientManager = new ClientManager($database);
 $allClients = $clientManager->getAllClients();
-// // var_dump($allClients); affiche un array de l'objet clients
-// foreach ($allClients as $unClient) {
-//     echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
-// }
-// // Exercice 3
-// $twentyClients = $clientManager->get20Clients();
-// foreach ($twentyClients as $unClient) {
-//     echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
-// }
-// // Exercice 5
-// $findClients = $clientManager->getClientByName("M");
-// foreach ($findClients as $unClient) {
-//     echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
-// }
-
-$findCC = $clientManager->get1Client();
-var_dump($findCC);
+// var_dump($allClients); affiche un array de l'objet clients
+foreach ($allClients as $unClient) {
+    echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
+}
+// Exercice 3
+$twentyClients = $clientManager->get20Clients();
+foreach ($twentyClients as $unClient) {
+    echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
+}
+// Exercice 5
+$findClients = $clientManager->getClientByName("M");
+foreach ($findClients as $unClient) {
+    echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
+}
+// Exercice bonus
+// $findCC = $clientManager->get1Client();
+// var_dump($findCC);
