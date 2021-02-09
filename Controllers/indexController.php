72395df6c2f39712987f6509cd8f7eaf75f384f6
sorayacruchet <soraya.cruchet@gmail.com>
2021-02-09 17:11:27 +0100

    exerci 7 maj condition

--- Controllers/indexController.php
+++ Controllers/indexController.php
@@ -7,45 +7,65 @@ $br = "<br>";
 
 $database = new Database();
 $clientManager = new ClientManager($database);
+
 // // Exercice 1
-// // Contient connexion PDO
-// $allClients = $clientManager->getAllClients();
-// // var_dump($allClients); affiche un array de l'objet clients
-// foreach ($allClients as $unClient) {
-//     echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
-// }
+// Contient connexion PDO
+$allClients = $clientManager->getAllClients();
+// var_dump($allClients); affiche un array de l'objet clients
+foreach ($allClients as $unClient) {
+    echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
+}
+
 // // Exercice 3
-// $twentyClients = $clientManager->get20Clients();
-// foreach ($twentyClients as $unClient) {
-//     echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
-// }
+$twentyClients = $clientManager->get20Clients();
+foreach ($twentyClients as $unClient) {
+    echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
+}
+
 // // Exercice 5
-// $findClients = $clientManager->getClientByName("M");
-// foreach ($findClients as $unClient) {
-//     echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
-// }
+$findClients = $clientManager->getClientByName("M");
+foreach ($findClients as $unClient) {
+    echo "Nom & Prénom du client : " . $unClient->getLastName() . " " . $unClient->getFirstName() . $br;
+}
 
 // // // Exercice bonus
-// // // $findCC = $clientManager->get1Client();
-// // // var_dump($findCC);
+$findCC = $clientManager->get1Client();
+var_dump($findCC);
 
 // // Exercice 4
-// $findLoyalClients = $clientManager->getLoyaltyCard();
-// foreach ($findLoyalClients as $unClient) {
-//     echo "Nom & Prénom du client : " . $unClient['lastName'] . " " . $unClient['firstName'] . $br;
-// }
+$findLoyalClients = $clientManager->getLoyaltyCard();
+foreach ($findLoyalClients as $unClient) {
+    echo "Nom & Prénom du client : " . $unClient['lastName'] . " " . $unClient['firstName'] . $br;
+}
 
 
 $showManager = new ShowManager($database);
 // Exercice 2 : filtre sur le genre de spectacles
-// $showTypes = $showManager->getShowTypes();
-// echo "Les types de spectacles possibles sont : " . $br;
-// foreach ($showTypes as $showType) {
-//     echo "* " . $showType['type'] . $br;
-// }
+$showTypes = $showManager->getShowTypes();
+echo "Les types de spectacles possibles sont : " . $br;
+foreach ($showTypes as $showType) {
+    echo "* " . $showType['type'] . $br;
+}
 
 // Exercice 6
 $allShows = $showManager->getAllShows();
 foreach ($allShows as $show) {
-        echo "🎞 A l'affiche : " . $show->getTitle() . " interprété par " . $show->getPerformer() . " le " . $show->getDate() . " à " . $show->getStartTime() . $br;
+    echo "🎞 A l'affiche : " . $show->getTitle() . " interprété par " . $show->getPerformer() . " le " . $show->getDate() . " à " . $show->getStartTime() . $br;
+}
+
+// Exercice 7
+$allDataClients = $clientManager->getAllClients();
+// var_dump($allDataClients); 
+
+foreach ($allDataClients as $dataClient) {
+    if ($dataClient->getCard() == 1) {
+        echo "Nom & Prénom du client : " . $dataClient->getLastName() . " " . $dataClient->getFirstName() . $br
+            . "Date de naissance : " . $dataClient->getBirthDate() . $br
+            . "Carte de fidélité : Oui." . $br
+            . "Numéro de carte : " . $dataClient->getCardNumber() . $br . $br;
+    } else {
+        echo "Nom & Prénom du client : " . $dataClient->getLastName() . " " . $dataClient->getFirstName() . $br
+            . "Date de naissance : " . $dataClient->getBirthDate() . $br
+            . "Pas de carte enregistrée." . $br . $br;
     }
+}
